877a1750fd32649fcfc221551780b3672d678026
mzhou <zmmbreeze0825@gmail.com>
2012-11-14 23:34:51 +0800

    修复分割线样式适应小屏幕，修改文档

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ lessc ./src/puretag.less > ./bin/puretag.css
 lessc ./src/markdown.less > ./bin/markdown.css
 lessc ./src/wordpress.less > ./bin/wordpress.css
 lessc ./src/blogin.less > ./bin/blogin.css
+lessc ./src/default.less > ./bin/default.css
