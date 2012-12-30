382f7985bc2377154d91cbfb632890c5bfe85c62
mzhou <zmmbreeze0825@gmail.com>
2012-12-30 19:32:16 +0800

    fix bug

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-lessc ./src/puretag.config.less > ./bin/puretag.config.css
-lessc ./src/markdown.config.less > ./bin/markdown.config.css
-lessc ./src/wordpress.config.less > ./bin/wordpress.config.css
-lessc ./src/blogin.config.less > ./bin/blogin.config.css
-lessc ./src/tblog.config.less > ./bin/tblog.config.css
-lessc ./src/default.less > ./bin/default.css
+lessc ./src/puretag.config.less > ./bin/puretag.css
+lessc ./src/markdown.config.less > ./bin/markdown.css
+lessc ./src/wordpress.config.less > ./bin/wordpress.css
+lessc ./src/blogin.config.less > ./bin/blogin.css
+lessc ./src/tblog.config.less > ./bin/tblog.css
+lessc ./src/default.config.less > ./bin/default.css
