a22151d16fd5db18cab0e52ebdf4ab1a11c1597e
zmmbreeze <zmmbreeze0825@gmail.com>
2013-06-18 11:28:54 +0800

    修改build脚本，修改文档中的bug，build

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 纯HTML 选择器为 body
-lessc -x ./src/puretag.config.less > ./bin/puretag.css
+lessc -x `pwd`/src/puretag.config.less > ./bin/puretag.css
 # 选择器为 .post
-lessc -x ./src/wordpress.config.less > ./bin/wordpress.css
+lessc -x `pwd`/src/wordpress.config.less > ./bin/wordpress.css
 # 选择器为 .markdown
-lessc -x ./src/blogin.config.less > ./bin/blogin.css
+lessc -x `pwd`/src/blogin.config.less > ./bin/blogin.css
 # 选择器为 body
-lessc -x ./src/tblog.config.less > ./bin/tblog.css
+lessc -x `pwd`/src/tblog.config.less > ./bin/tblog.css
 # 选择器为 .entry
-lessc -x ./src/default.config.less > ./bin/entry.css
+lessc -x `pwd`/src/default.config.less > ./bin/entry.css
 
