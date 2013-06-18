a52511349e35149be52b40339b3a1a1df8e34e15
mzhou <zmmbreeze0825@gmail.com>
2013-06-18 22:12:51 +0800

    update

---
+++ src/less-config.js
@@ -0,0 +1,8 @@
+(function($, less) {
+    var LessConfig = function() {};
+    var LessRender = function() {};
+    var LessConfiger = function(config) {
+        this.config = config;
+    };
+    LessConfiger.prototype.rend = function() {};
+})(jQuery, less);
