7c2c0291bc9c00c7e60138899844756221db656d
zmmbreeze <zmmbreeze0825@gmail.com>
2014-01-23 14:55:16 +0800

    更新blockquote样式（加左边框）；更新code样式（减小字体大小）；修证优化文档

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 纯HTML 选择器为 body
-lessc -x `pwd`/src/puretag.config.less > ./bin/puretag.css
+/usr/bin/lessc -x `pwd`/src/puretag.config.less > ./bin/puretag.css
 # 选择器为 .post
-lessc -x `pwd`/src/wordpress.config.less > ./bin/wordpress.css
+/usr/bin/lessc -x `pwd`/src/wordpress.config.less > ./bin/wordpress.css
 # 选择器为 .markdown
-lessc -x `pwd`/src/blogin.config.less > ./bin/blogin.css
+/usr/bin/lessc -x `pwd`/src/blogin.config.less > ./bin/blogin.css
 # 选择器为 .entry
-lessc -x `pwd`/src/default.config.less > ./bin/entry.css
+/usr/bin/lessc -x `pwd`/src/default.config.less > ./bin/entry.css
 # 选择器为 .entry
-lessc -x `pwd`/src/word.config.less > ./bin/word.css
+/usr/bin/lessc -x `pwd`/src/word.config.less > ./bin/word.css
 
