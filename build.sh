dadc787b54dac8501a591fe0cfa315f90dc9a59a
zmmbreeze <zmmbreeze0825@gmail.com>
2014-01-25 14:09:14 +0800

    更新blockquote的左内边距

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 纯HTML 选择器为 body
-/usr/bin/lessc -x `pwd`/src/puretag.config.less > ./bin/puretag.css
+lessc -x `pwd`/src/puretag.config.less > ./bin/puretag.css
 # 选择器为 .post
-/usr/bin/lessc -x `pwd`/src/wordpress.config.less > ./bin/wordpress.css
+lessc -x `pwd`/src/wordpress.config.less > ./bin/wordpress.css
 # 选择器为 .markdown
-/usr/bin/lessc -x `pwd`/src/blogin.config.less > ./bin/blogin.css
+lessc -x `pwd`/src/blogin.config.less > ./bin/blogin.css
 # 选择器为 .entry
-/usr/bin/lessc -x `pwd`/src/default.config.less > ./bin/entry.css
+lessc -x `pwd`/src/default.config.less > ./bin/entry.css
 # 选择器为 .entry
-/usr/bin/lessc -x `pwd`/src/word.config.less > ./bin/word.css
+lessc -x `pwd`/src/word.config.less > ./bin/word.css
 
