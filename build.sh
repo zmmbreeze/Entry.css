6c100373fd16d73048a6618690c4b52c0bae1260
zmmbreeze <zmmbreeze0825@gmail.com>
2014-10-30 23:29:33 +0800

    1. 移除blogin配置 2. 更新默认配置     - `@h2-text-align`, center => left     - `@use-title-number`, false =< true

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@
 lessc -x `pwd`/src/puretag.config.less > ./bin/puretag.css
 # 选择器为 .post
 lessc -x `pwd`/src/wordpress.config.less > ./bin/wordpress.css
-# 选择器为 .markdown
-lessc -x `pwd`/src/blogin.config.less > ./bin/blogin.css
 # 选择器为 .entry
 lessc -x `pwd`/src/default.config.less > ./bin/entry.css
 # 选择器为 .entry
